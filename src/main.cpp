434e0fc619257468787f4b233da8e98ae1f8597a
Jeff Backus <jeff.backus@gmail.com>
2015-02-27 21:59:27 -0500

    * Added a copy of the GPLv3. * Added license header to all current code files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,26 @@
+/*
+ *    SimpleFirewallClient is a GUI tool for configuring the a Linux firewall.
+ *    (c) 2015 Jeff Backus <jeff.backus@gmail.com>
+ *
+ *    This file is part of SimpleFirewallClient.
+ *
+ *    SimpleFirewallClient is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    SimpleFirewallClient is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with SimpleFirewallClient.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    Home Page:
+ *    https://github.com/jsbackus/SimpleFirewallClient
+ */
+
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDebug>
 
